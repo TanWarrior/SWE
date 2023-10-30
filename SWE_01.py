d40df61f3b7f7e1acfbc56ecfa0429771c4f3eeb
TanWarrior <145176452+TanWarrior@users.noreply.github.com>
2023-10-30 19:34:32 +0530

    Add files via upload

---
+++ SWE_01.py
@@ -0,0 +1,9 @@
+# HackerRank Question
+# The provided code stub reads and integer, , from STDIN. For all non-negative i<n integers , print i^2.
+# n = int(input("Enter number:"))
+n=5
+i=0
+while(i<n):
+    print(i*i)
+    i=i+1
+    
